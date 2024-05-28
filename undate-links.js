8d56e6f26367dcab0bc3b6abb48c0ce2ca7a181a
Roman <brysommer1@gmail.com>
2024-05-28 14:15:03 +0300

    update

--- undate-links.js
+++ undate-links.js
@@ -6,7 +6,7 @@ import { findAllAncNames, updatelinkByDrug_id } from './models/ancNomenclatura.j
 const updateLinks = async () => {
     const ancNames = await findAllAncNames();
     console.log(ancNames[0].link)
-    const nameNeedLink = ancNames.filter(name => name.link == false);
+    const nameNeedLink = ancNames.filter(name => name.link == null);
     console.log(nameNeedLink.lenght)
     for (link of nameNeedLink) {
         const getXMLPrice = async(products) => {
