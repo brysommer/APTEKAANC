c6385966b29db7265e192ced033b195a915a60fa
Roman <brysommer1@gmail.com>
2024-05-28 13:38:27 +0300

    update import

--- util/undate-links.js
+++ util/undate-links.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { findAllAncNames, updatelinkByDrug_id } from './models/ancNomenclatura.js';
+import { findAllAncNames, updatelinkByDrug_id } from '../models/ancNomenclatura.js';
 
 
 
